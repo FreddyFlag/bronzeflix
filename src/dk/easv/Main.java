5ee20dac0eb8b9b40d040b462ea0878d8d899f02
FreddyFlag <31618798+FreddyFlag@users.noreply.github.com>
2023-02-24 07:51:01 +0100

    Final push hopefully

--- src/dk/easv/Main.java
+++ src/dk/easv/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("presentation/view/LogIn.fxml"));
-        primaryStage.setTitle("BronzeFlix");
+        primaryStage.setTitle("Bronzeflix Login Page");
         // primaryStage.setFullScreen(true);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
