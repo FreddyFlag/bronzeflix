64b4793c11ef90a49ad5dca2493128b0832416e1
FreddyFlag <31618798+FreddyFlag@users.noreply.github.com>
2023-02-24 03:16:36 +0100

    funny popup

--- src/dk/easv/presentation/controller/LogInController.java
+++ src/dk/easv/presentation/controller/LogInController.java
@@ -3,6 +3,7 @@ package dk.easv.presentation.controller;
 import dk.easv.presentation.model.AppModel;
 import io.github.palexdev.materialfx.controls.MFXPasswordField;
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,10 +14,13 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+
 
 public class LogInController implements Initializable {
     @FXML private MFXPasswordField passwordField;
@@ -29,37 +33,45 @@ public class LogInController implements Initializable {
         model = new AppModel();
     }
 
+
+
     public void logIn(ActionEvent actionEvent) {
         model.loadUsers();
         model.loginUserFromUsername(userId.getText());
         if(model.getObsLoggedInUser()!=null){
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/dk/easv/presentation/view/MainApp.fxml"));
-            Parent root = loader.load();
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.setTitle("BronzeFlix 0.3 Beta");
-            stage.show();
-            MainAppController controller = loader.getController();
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/dk/easv/presentation/view/MainApp.fxml"));
+                Parent root = loader.load();
+                Stage stage = new Stage();
+                stage.setScene(new Scene(root));
+                stage.setTitle("BronzeFlix 0.3 Beta");
+                stage.show();
+                MainAppController controller = loader.getController();
+                controller.setModel(model);
 
-            controller.setModel(model);
+                // Display pop-up message
+                Alert alert = new Alert(Alert.AlertType.INFORMATION, "Welcome to the bronze tier of netflix without the ability to watch movies... Enjoy!");
+                alert.setResizable(true);
+                alert.setTitle("BronzeFlix 0.3 Beta");
+                alert.setHeaderText("Welcome!");
+                alert.getDialogPane().setMinWidth(400);
+                alert.getDialogPane().setMinHeight(200);
+                alert.show();
 
+               // Close the dialog after 8 seconds
+                PauseTransition delay = new PauseTransition(Duration.seconds(8));
+                delay.setOnFinished(event -> alert.close());
+                delay.play();
 
-        } catch (IOException e) {
-            e.printStackTrace();
-            Alert alert = new Alert(Alert.AlertType.ERROR, "Could not load App.fxml");
-            alert.showAndWait();
-        }
-
+            } catch (IOException e) {
+                e.printStackTrace();
+                Alert alert = new Alert(Alert.AlertType.ERROR, "Could not load App.fxml");
+                alert.showAndWait();
+            }
         }
         else{
             Alert alert = new Alert(Alert.AlertType.ERROR, "Wrong username or password");
             alert.showAndWait();
         }
-    }
+    }}
 
-    public void signUp(ActionEvent actionEvent) {
-        System.out.println("Sign-Up");
-    }
-
-}
